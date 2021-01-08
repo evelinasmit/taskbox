e60118107a5e6032344dbaa547158f865cd6519a
Evelina Smitaite <evelina.smitaite@reducer.co.uk>
2021-01-08 22:42:26 +0000

    GitHub Pages deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+set -e
+# build
+yarn run build
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:evelinasmit/taskbox.git main:gh-pages
+
+cd -
